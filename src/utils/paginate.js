49091fb82cc38a0182ac78234a6af4aa97922da9
LenaNguyen27 <nguyen.lena27@hotmail.com>
2019-04-21 15:36:48 -0400

    fixed pagination bug

--- src/utils/paginate.js
+++ src/utils/paginate.js
@@ -1,7 +1,8 @@
 //import _ from './lodash';
 
 export function paginate(items, pageNumber, pageSize) {
-    const startIndex = (pageNumber - 1) * pageNumber;
+    const startIndex = (pageNumber - 1) * pageSize;
+    console.log(items, startIndex);
     return items.slice(startIndex, startIndex + pageSize);
 
     //how to paginate using lodash:
